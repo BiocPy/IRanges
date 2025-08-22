4ef8774f7e5cdf00512ac041d7ca44bcf86d0c4c
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2025-08-22 21:54:39 +0530

    update again

--- tests/test_IRanges.py
+++ tests/test_IRanges.py
@@ -227,8 +227,8 @@ def test_index():
     assert len(orig) != len(mod)
 
     i2 = IRanges(start=np.array([105, 205, 305], dtype=np.int32), width=np.array([20, 20, 20], dtype=np.int32))
+    i2.intersect_ncls(i1, delete_index=False) # perform some operation to check if the index is cached
     i3 = i2.set_start(i2.start + 90)
-    i3.delete_nclist_index()
     mod2 = i1.intersect_ncls(i3)
     assert len(orig) != len(mod2)
     assert len(mod) == len(mod2)
